d24cde046e26640c1e78c3e9d6332c12a2e8c012
Yuri Alexeev <alexseevyi@gmail.com>
2022-08-28 16:52:41 +0400

    Fixing after review

--- resources/views/layouts.blade.php
+++ resources/views/layouts.blade.php
@@ -8,7 +8,6 @@
   <title>Главная страница</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.min.js"></script>
-  <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
 </head>
 
 <body>
--- resources/views/news.blade.php
+++ resources/views/news.blade.php
@@ -82,22 +82,13 @@
     @endforeach
 </div>
 
-
-<?php
-$pageCount = intdiv($newsData['totalResults'], old('pageSize'));
-
-if ($newsData['totalResults'] % old('pageSize') != 0) {
-    $pageCount += 1;
-}
-?>
-
 <nav aria-label="Page navigation example">
     <ul class="pagination justify-content-center">
         <li class="page-item pages__item" value="1"><div class="page-link">First</div></li>
-        @for ($pageNumber = 1; $pageNumber <= $pageCount; $pageNumber++) 
+        @for ($pageNumber = 1; $pageNumber <= $newsData['pageCount']; $pageNumber++) 
             <li class="page-item pages__item" name="page" id="page" value="{{$pageNumber}}"><div class="page-link">{{$pageNumber}}</div></li>
         @endfor
-        <li class="page-item pages__item" value="{{$pageCount}}"><div class="page-link">Last</div></li>
+        <li class="page-item pages__item" value="{{$newsData['pageCount']}}"><div class="page-link">Last</div></li>
     </ul>
 </nav>
 
